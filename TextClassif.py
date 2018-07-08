04947095e49994c75314b6640c3d6303689b4727
shouguoyang <891584158@qq.com>
2018-07-08 17:48:00 +0800

    init

---
+++ TextClassif.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2018/7/8 上午11:49
+# @Author  : 杨寿国
+# @mail    : 891584158@qq.com
+# @File    : data_helper.py
+# @intro: 神经网络模板
+import tensorflow as tf
+
+
+class TextCNN(object):
+    def __init__(self
+                 , seq_len, seq_width, num_class, vocabsize, embedding_size, filter_sizes, num_filters):
+        pass
+
+        self.input_x = tf.placeholder(tf.float32, [None, seq_len, seq_width], name='inputx')
+        self.input_y = tf.placeholder(tf.float32, [None, num_class], name='inputy')
+        self.dropout_keep_prob = tf.placeholder(tf.float32, name='dropout_keep_prob')
+
+        with tf.device('/cpu:0'), tf.name_scope('embedding'):
+            # W = tf.Variable(
+            #     tf.random_uniform([vocabsize, embedding_size], -1.0, 1.0), name='W'
+            # )
+            #
+            # # The result of the embedding operation is a 3-dimensional tensor of shape [None, sequence_length, embedding_size]
+            # self.embedding_chars = tf.nn.embedding_lookup(W, self.input_x)
+            # self.embedding_chars_expanded = tf.expand_dims(self.embedding_chars, -1)
+            self.embedding_chars_expanded = tf.expand_dims(self.input_x, -1)
+
+        # Convolutional and max-pooling layers
+        # use filters of differents sizes
+        pooled_outputs = []
+        for i, filter_size in enumerate(filter_sizes):
+            with tf.name_scope('conv-maxpool-%s' % filter_size):
+                # Conv layer
+                filter_shape = [filter_size, embedding_size, 1, num_filters]
+                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name='W')
+                b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name='b')
+                conv = tf.nn.conv2d(self.embedding_chars_expanded,
+                                    W,
+                                    strides=[1, 1, 1, 1],
+                                    padding="VALID",
+                                    name='conv')
+
+                h = tf.nn.relu(tf.nn.bias_add(conv, b), name='relu')
+
+                pooled = tf.nn.max_pool(h, ksize=[1, seq_len - filter_size + 1, 1, 1],
+                                        strides=[1, 1, 1, 1],
+                                        padding="VALID",
+                                        name='pool')
+                pooled_outputs.append(pooled)
+        # Combine all the pooled features
+        num_filters_total = num_filters * len(filter_sizes)
+        self.h_pool = tf.concat(axis=3, values=pooled_outputs)
+        self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
+
+        with tf.name_scope('dropout'):
+            self.h_drop = tf.nn.dropout(self.h_pool_flat, self.dropout_keep_prob)
+
+        with tf.name_scope('output'):
+            W = tf.Variable(tf.truncated_normal([num_filters_total, num_class], stddev=0.1), name='W')
+            b = tf.Variable(tf.constant(0.1, shape=[num_class]), name="b")
+            self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
+            self.predictions = tf.argmax(self.scores, 1, name="predictions")
+
+        with tf.name_scope('loss'):
+            losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores, labels=self.input_y)
+            self.loss = tf.reduce_mean(losses)
+
+        with tf.name_scope('accuracy'):
+            correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_y, 1))
+            self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
---
+++ data_helper.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2018/7/8 上午10:33
+# @Author  : 杨寿国
+# @mail    : 891584158@qq.com
+# @File    : data_helper.py
+# @intro: 读取各个优化级别的二进制文件，并且自动附加标签，从O0 - Os 依次对应 0~5
+import os
+import numpy as np
+
+
+class DataHelper(object):
+    def __init__(self):
+        self.rowbyte = 4
+        pass
+
+    def get_all_data(self, directory='./dataset/ARM'):
+
+        all_result = []
+        all_result += self.get_certain_compile_level_with_label(os.path.join(directory, 'O0'), label=0)
+        all_result += self.get_certain_compile_level_with_label(os.path.join(directory, 'O1'), label=1)
+        all_result += self.get_certain_compile_level_with_label(os.path.join(directory, 'O2'), label=2)
+        all_result += self.get_certain_compile_level_with_label(os.path.join(directory, 'O3'), label=3)
+        all_result += self.get_certain_compile_level_with_label(os.path.join(directory, 'Os'), label=4)
+
+        return np.array(all_result)
+
+    def get_certain_compile_level_with_label(self, directory, label):
+        # directory: 某一优化级别的二进制文件所在目录
+        # label:该类二进制文件对应标签
+
+        result = []
+        # 迭代访问目录下每个文件
+        for file in os.listdir(directory):
+            result.append([self.read_binary_from_file(os.path.join(directory, file)), label])
+
+        return result
+
+    def load_data_and_labels(self, dir):
+        data = self.get_all_data(dir)
+        train_data = np.array(list(data[:, 0]), dtype=np.float32)
+        return train_data, data[:, 1]
+
+    def read_binary_from_file(self, file_path, rowbyte=4, filesize=4096):
+        # file_path: 二进制文件所在目录
+        # rowbyte: 每一行的字节数
+        # filesize : 文件大小
+        # 返回一个矩阵 N*32矩阵 N = 4096/32 = 128
+
+        allbytevalue = []
+        with open(file_path, 'rb') as f:
+            allbytevalue = list(bytearray(f.read(filesize)))
+
+        byte_array = np.array(allbytevalue)
+        return byte_array.reshape(-1, rowbyte)
+
+    def batch_iter(self, data, batch_size, num_epochs, shuffle=True):
+        """
+        Generates a batch iterator for a dataset.
+        """
+        data = np.array(data)
+        data_size = len(data)
+        num_batches_per_epoch = int((len(data) - 1) / batch_size) + 1
+        for epoch in range(num_epochs):
+            # Shuffle the data at each epoch
+            if shuffle:
+                shuffle_indices = np.random.permutation(np.arange(data_size))
+                shuffled_data = data[shuffle_indices]
+            else:
+                shuffled_data = data
+            for batch_num in range(num_batches_per_epoch):
+                start_index = batch_num * batch_size
+                end_index = min((batch_num + 1) * batch_size, data_size)
+                yield shuffled_data[start_index:end_index]
+
+
+def test():
+    dh = DataHelper()
+    data = dh.get_all_data("./dataset/ARM/")
+    input = data[:, 0]
+    labels = data[:, 1]
+    single_in = input[0]
+    print(labels)
+
+
+if __name__ == '__main__':
+    test()
