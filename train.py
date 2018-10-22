7246d164c016d999822c48bdb77344a86964d68a
shouguoyang <891584158@qq.com>
2018-10-22 12:35:51 +0800

    single class acc compute v2

--- train.py
+++ train.py
@@ -1,8 +1,6 @@
 import tensorflow as tf
 import numpy as np
 import os
-import time
-import datetime
 from data_helper import DataHelper
 from Optimization_Class import TextCNN
 
@@ -14,12 +12,12 @@ tf.flags.DEFINE_string("data_file", "../dataset_decreaseO23/",
 #                        "Data source for the negative data.")
 
 # Model Hyperparameters
-tf.flags.DEFINE_integer("embedding_dim", 4, "Dimensionality of character embedding (default: 4)")
+tf.flags.DEFINE_integer("embedding_dim", 8, "Dimensionality of character embedding (default: 4)")
 tf.flags.DEFINE_string("filter_sizes", "3,4,64,100", "Comma-separated filter sizes (default: '3,4,5,6')")
 tf.flags.DEFINE_integer("num_filters", 64, "Number of filters per filter size (default: 128)")
 tf.flags.DEFINE_float("dropout_keep_prob", 0.8, "Dropout keep probability (default: 0.8)")
 tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularization lambda (default: 0.0)")
-tf.flags.DEFINE_integer("hidden_dim", 64, "hidden layer dimension default(500)")
+tf.flags.DEFINE_integer("hidden_dim", 1024, "hidden layer dimension default(500)")
 
 # Training parameters
 tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
